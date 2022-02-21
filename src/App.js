a91b6f88a0ecd788c306c0709a9a1df2f3ddd24b
Scott Nelson <snelson.seattle@gmail.com>
2022-02-21 12:25:30 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      Advice # Advice text goes here
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="#" target="_blank" rel="noreferrer">
+          Scott E. Nelson
+        </a>
+        .
+      </div>
+    </div>
+  );
+}
+
+export default App;
